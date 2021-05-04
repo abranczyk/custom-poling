561d52ce2c4b374f060fba1d869fc064652d7438
Aggie <abranczyk@gmail.com>
2021-05-04 17:53:49 -0400

    refactored AmplitudeFunction

--- custom_poling/utils/amplitude_function.py
+++ custom_poling/utils/amplitude_function.py
@@ -12,11 +12,8 @@ class AmplitudeFunction:
         self.z0 = z0
 
     def compute(self):
-        phase_factor = []
+        phase = lambda k1,k2,za,z0a: 1j*(np.exp(-1j*(k1-k2)*za)-np.exp(-1j*(k1-k2)*z0a))/(k1-k2)
+        phase_factor = phase(self.k_array,self.k,self.z,self.z0)
         self.dk = self.k_array[1]-self.k_array[0]
-        phase_factor = []
-        for k1 in self.k_array:
-            phase_factor += [PhaseFactor(k1,self.k,self.z,self.z0).compute()]
-        phase_factor = np.array(phase_factor)
         kernel = self.pmf * phase_factor
         return np.sum(kernel) * self.dk
