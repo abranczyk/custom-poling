b063cd1314da501bb450f76d12b35a10315a51aa
Aggie <abranczyk@gmail.com>
2021-05-23 18:05:37 -0400

    added missing 2pi to amplitude

--- custom_poling/utils/pmf.py
+++ custom_poling/utils/pmf.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def pmf(domain_walls, domain_configuration, k):
-    pmf_one_domain = lambda z1,z2:1j*(np.exp(-1j*k*z2)-np.exp(-1j*k*z1))/k
+    pmf_one_domain = lambda z1,z2:2*np.pi*1j*(np.exp(-1j*k*z2)-np.exp(-1j*k*z1))/k
     pmf = 0
     for idx in range(len(domain_configuration)):
         pmf = pmf + domain_configuration[idx]*pmf_one_domain(domain_walls[idx],domain_walls[idx+1])
