d6705138d0fa4f10c81cc8a05583564b4e435fd2
Aggie <abranczyk@gmail.com>
2021-12-08 20:45:24 -0500

    updated definition of pmf

--- custom_poling/utils/pmf.py
+++ custom_poling/utils/pmf.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def pmf(domain_walls, domain_configuration, k):
-    pmf_one_domain = lambda z1,z2:2*np.pi*1j*(np.exp(-1j*k*z2)-np.exp(-1j*k*z1))/(k*2*np.pi)
+    pmf_one_domain = lambda z1,z2:2*np.pi*1j*(np.exp(1j*k*z1)-np.exp(1j*k*z2))/(k*2*np.pi)
     pmf = 0
     for idx in range(len(domain_configuration)):
         pmf = pmf + domain_configuration[idx]*pmf_one_domain(domain_walls[idx],domain_walls[idx+1])
