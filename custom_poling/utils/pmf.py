352fd8750299dbedf941114fc6b56f969de70b24
Aggie Branczyk <abranczyk@gmail.com>
2024-01-02 07:14:36 -0500

    relable k to k_array for clarity

--- custom_poling/utils/pmf.py
+++ custom_poling/utils/pmf.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-def pmf(domain_walls, domain_configuration, k):
-    pmf_one_domain = lambda z1,z2:2*np.pi*1j*(np.exp(1j*k*z1)-np.exp(1j*k*z2))/(k*2*np.pi)
+def pmf(domain_walls, domain_configuration, k_array):
+    pmf_one_domain = lambda z1,z2:2*np.pi*1j*(np.exp(1j*k_array*z1)-np.exp(1j*k_array*z2))/(k_array*2*np.pi)
     pmf = 0
     for idx in range(len(domain_configuration)):
         pmf = pmf + domain_configuration[idx]*pmf_one_domain(domain_walls[idx],domain_walls[idx+1])
