039f7d3c602f5a203733e887dab1d7218f9c26d7
Aggie Branczyk <abranczyk@gmail.com>
2024-01-02 07:50:33 -0500

    added value error for k_array elements

--- custom_poling/utils/pmf.py
+++ custom_poling/utils/pmf.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def pmf(domain_walls, domain_configuration, k_array):
+    """Returns the phasematching function (PMF) as a function of k for a given domain_configuration."""
+    if np.any(k_array==0.):
+        raise ValueError('all k_array elements must be non-zero')
     pmf_one_domain = lambda z1,z2:2*np.pi*1j*(np.exp(1j*k_array*z1)-np.exp(1j*k_array*z2))/(k_array*2*np.pi)
     pmf = 0
     for idx in range(len(domain_configuration)):
