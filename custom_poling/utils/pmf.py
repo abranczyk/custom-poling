7b9da0d4abe6e79a6d7716c22bccb1f0995ee758
Aggie Branczyk <abranczyk@gmail.com>
2024-01-02 08:26:41 -0500

    updated ValueError

--- custom_poling/utils/pmf.py
+++ custom_poling/utils/pmf.py
@@ -3,7 +3,7 @@ import numpy as np
 def pmf(domain_walls, domain_configuration, k_array):
     """Returns the phasematching function (PMF) as a function of k for a given domain_configuration."""
     if np.any(k_array==0.):
-        raise ValueError('all k_array elements must be non-zero')
+        raise ValueError('Sorry, no numbers equal to zero inside k_array')
     pmf_one_domain = lambda z1,z2:2*np.pi*1j*(np.exp(1j*k_array*z1)-np.exp(1j*k_array*z2))/(k_array*2*np.pi)
     pmf = 0
     for idx in range(len(domain_configuration)):
